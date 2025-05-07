ec1d75ee3770ab8a812acffafda8b576bf9703cd
sawaniThiwandika <thiwandika.whs02@gmail.com>
2025-05-07 22:52:00 +0530

    rename package routes

--- Server.ts
+++ Server.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db';
-import userRoutes from './routers/UserRoute';
-import classRoutes from './routers/ClassRoute';
+import userRoutes from './routes/UserRoute';
+import classRoutes from './routes/ClassRoute';
 
 const app = express();
 const PORT = 3000;
